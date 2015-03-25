83cc9051a9dbd84b13339d4fd523fbe9390b160d
Stewart Newnham <git@newnham.com>
2015-03-25 22:08:13 +0000

    AllTests - Added TestReadString . Suite Passes

--- cw5/src/test/AllTests.java
+++ cw5/src/test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ testReadUntil.class, TestSkipSpace.class })
+@SuiteClasses({ testReadUntil.class, TestSkipSpace.class, TestReadString.class })
 public class AllTests {
 
 }
