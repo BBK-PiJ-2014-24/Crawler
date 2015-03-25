33c41255fd1938c2a6117abaf91e24b737b0755e
Stewart Newnham <git@newnham.com>
2015-03-25 19:02:30 +0000

    HTMLread Impl - First Implementation of skipSpace().

--- cw5/src/implementation/HTMLreadImpl.java
+++ cw5/src/implementation/HTMLreadImpl.java
@@ -30,6 +30,38 @@ public class HTMLreadImpl implements HTMLread{
 		
 		return false;
 	}
+
+	@Override
+	public char skipSpace(InputStream inpStream, char ch) {
+		
+		char lowChar = '0';
+		if(Character.isAlphabetic(ch))
+			 ch = Character.toLowerCase(ch);
+		
+		int i = 0;
+		try {
+			while((i = inpStream.read()) != -1){
+				char c = (char) i;
+				char charBefore = 'b'; 
+				if (Character.isAlphabetic(c))
+					c = Character.toLowerCase(lowChar);
+				
+				if( c == ch)
+					return lowChar;   // found char returns lowChar 
+				else if( charBefore == ' ' && c != ' ')
+					return c;  // found char after white space
+				else 
+					charBefore = c;	
+			}
+		} 
+		catch (IOException e) {
+			System.out.println("ERROR WITH INPUTSTREAM.READ()");
+			e.printStackTrace();
+		}
+		
+		return ' '; 	// returns whiteSpace if not found char and not found char after whiteSpace
+		
+	}
 	
 
 	
