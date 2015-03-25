abeabc89222fe96cc08705742ccfcd1ce8b61f4f
Stewart Newnham <git@newnham.com>
2015-03-25 14:17:16 +0000

    HTMLread Impl - First Implementation of HTMLread.readUntil()

---
+++ cw5/src/implementation/HTMLreadImpl.java
@@ -0,0 +1,34 @@
+package implementation;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import iinterace.HTMLread;
+
+public class HTMLreadImpl implements HTMLread{
+
+	@Override
+	public boolean readUntil(InputStream inpStream, char c1, char c2) {
+		
+		if(Character.isAlphabetic(c1))
+			 c1 = Character.toLowerCase(c1);
+		if(Character.isAlphabetic(c2))
+			c2 = Character.toLowerCase(c1);
+		
+		int i = 0;
+		try {
+			while((i = inpStream.read()) != -1){
+				if((char) i == c1)
+					return true;
+				else if((char) i == c2)
+					return false;
+			}
+		} catch (IOException e) {
+			System.out.println("ERROR WITH INPUTSTREAM.READ()");
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	
+}
