9933fd43f7bed27ea11785cc8b395cdd487a3853
Stewart Newnham <git@newnham.com>
2015-03-28 11:15:02 +0000

    WebCrawler iinterface - Refactor the crawl() so that it accepts a String parameter in order to pass URL link. Also temporarily change the return type to String for intial testing purposes

--- cw5/src/iinterace/WebCrawler.java
+++ cw5/src/iinterace/WebCrawler.java
@@ -5,6 +5,8 @@ public interface WebCrawler {
 	/**
 	 * crawl() method will open an HTTP connection to the starting URL and review the entire web page 
 	 * found there, saving all URL links found within the page to a temporary database table.
+	 * @parameter url - The URL to be Searched for Links
+	 * @return Returns a URL link found on the page
 	 */
-	 String crawl();
+	 String crawl(String url);
 }
