86e112dda5647a33078ba123ef10bf53c58f13a9
Stewart Newnham <git@newnham.com>
2015-03-29 13:02:26 +0100

    WebCrawler Test - Added Extra tests to determine if crawl() can differentiate between legal and illegal <a href = xxx.com formats?

--- cw5/src/test/WebCrawlerTest.java
+++ cw5/src/test/WebCrawlerTest.java
@@ -22,6 +22,8 @@ public class WebCrawlerTest {
 	WebCrawler wc;
 	String webPage1;
 	String webPage2;
+	String webPage3;
+	
 	File containerFile;
 	
 	
@@ -29,27 +31,45 @@ public class WebCrawlerTest {
 	public void setUp(){
 		wc = new WebCrawlerImpl();
 		webPage1 = "firstSearch.html";
-		webPage2 = "firstSearch2.html";
+		webPage2 = "href.html";    //"firstSearch2.html";
+		webPage3 = "href2.html";
 		containerFile = new File("Container.txt");
 	}
 	
 	/**
-	 * Test for first easy search of a web link in firstSearch.html
+	 * Test whether crawl() can detect a legal <a href = "xxxx.xxx">
 	 */
 	@Test
 	public void pickUpLinkTest1() {
 		String ans = "\"http://google.com\"";
-		String s = wc.crawl(webPage1);
-		assertEquals("Test crawl() can detect a normal looking link in HTML page: ", ans,s);
-	}
-	
-	@Test
-	public void pickUpLinkTest2() {
-		String ans = "\"http://google.com\"";
 		String s = wc.crawl(webPage2);
-		System.out.println("test2 " + s);
 		assertEquals("Test crawl() can detect a link (filled with white-space gaps) in HTML page: ", ans,s);
 	}
+	
+	/**
+	 * Test whether crawl() can differentiate between Legal and Illegal Link formats of the form <a href = "xxxx.xxx">
+	 */
+	@Test
+	public void pickUpLinkTest2() {
+		String ans = "\"http://bbc.co.uk\"";
+		String s = wc.crawl(webPage3);
+		assertEquals("Test crawl() can detect detect Legal and Illegal Link formats of the form <a href = xxxx. com>: "
+				, ans,s);		
+	}
+	
+	/**
+	 * Test for first easy search of a web link in a simple HTML mock up page (firstSearch.html)
+	 */
+    @Test
+	public void pickUpLinkTest3() {
+		String ans = "\"http://google.com\"";
+		String s = wc.crawl(webPage1);
+		assertEquals("Test crawl() can detect a normal looking link in HTML page: ", ans,s);
+	} 
+	
+
+	
+
 	
 	
 
