fba8d4f13af86b70e6d981568c8bf4bbc5b208b3
Stewart Newnham <git@newnham.com>
2015-04-02 17:58:47 +0100

    WebCrawler ExportTest - Test to check that getDatabase() correctly returns a text file that contains the right data links after multiple crawls. Also Checking Right Priority Order.

--- cw5/src/test/WebCrawlerExportTest.java
+++ cw5/src/test/WebCrawlerExportTest.java
@@ -26,6 +26,8 @@ import org.apache.commons.io.FileUtils;;
 public class WebCrawlerExportTest {
 
 	String webPage1;
+	String webPage2;
+	String webPage3;
 	WebCrawler wc;
 	File containerFile;
 	
@@ -33,6 +35,8 @@ public class WebCrawlerExportTest {
 	@Before
 	public void setUp(){
 		webPage1 = "file:href3.html";
+		webPage1 = "file:href4.html";
+		webPage1 = "file:href5.html";
 		containerFile = new File("Container.txt");
 		wc = new WebCrawlerImpl();
 	}
@@ -42,13 +46,13 @@ public class WebCrawlerExportTest {
 	 * data links in its temp database table file.
 	 */
 	@Test
-	public void compareTempDatabaseTest() {
+	public void compareTempDatabaseTest1() {
 		
 		
 		System.out.println("Start Test");
 		PriorityQueue<WebNode> q = wc.crawl(webPage1);
 		File database = wc.getDatabase();
-		File ans1 = new File("myDatabase1.txt");
+		File ans1 = new File("myDatabaseAnswers1.txt");
 	
 		try {
 			assertEquals("The files differ 1", 
@@ -60,10 +64,36 @@ public class WebCrawlerExportTest {
 		
 		catch (IOException e) {
 			e.printStackTrace();
-		}
+		}	
+	}
+	
+	/**
+	 * Test to check that getDatabase() correctly returns a text file that contains the right 
+	 * data links after multiple crawls.
+	 */
+	@Test
+	public void compareTempDatabaseTest2() {
 		
 		
+		System.out.println("Start Test");
+		wc.crawl(webPage1);  //1st crawl()
+		wc.crawl(webPage2);  // 2nd crawl()
+		PriorityQueue<WebNode> q = wc.crawl(webPage3);  //3rd crawl()
 		
+		File database = wc.getDatabase();
+		File ans1 = new File("myDatabaseAnswers2.txt");
+	
+		try {
+			assertEquals("The files differ 1", 
+				    FileUtils.readFileToString(ans1, "utf-8"), 
+				    FileUtils.readFileToString(database, "utf-8"));
+			
+			assertTrue("The files differ 2", FileUtils.contentEquals(ans1, database));
+		} 
+		
+		catch (IOException e) {
+			e.printStackTrace();
+		}	
 	}
 
 }
