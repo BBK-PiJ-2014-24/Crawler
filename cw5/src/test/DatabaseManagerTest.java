00ac67e170f07b26522da58fbeafd8279585cce6
Stewart Newnham <git@newnham.com>
2015-04-03 17:11:10 +0100

    DatabaseManager Test - Tests for retrieveNextWebNode(). a) test to see if retrieveNextWebNode() retrieves the correct link for processing from the Temporary URL table;
    b) Test Also that the database has been correctly amended so that the
    retrieved WebNode in the database has a priority setting at 0; 
    
    c)  Also Return Null on Empty database

--- cw5/src/test/DatabaseManagerTest.java
+++ cw5/src/test/DatabaseManagerTest.java
@@ -3,12 +3,14 @@ package test;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
 
 import iinterface.DatabaseManager;
 import iinterface.WebNode;
 import implementation.DatabaseManagerImpl;
 import implementation.WebNodeImpl;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,6 +22,10 @@ public class DatabaseManagerTest {
 	// ------
 	DatabaseManager dm;
 	File file;
+	File fileA;
+	File fileB;
+	File fileC;
+	
 	
 	@Before
 	public void setUp() throws Exception {
@@ -79,6 +85,54 @@ public class DatabaseManagerTest {
 		
 		x = dmNonEmpty.sizeOfTempTable();   // size of non-empty database
 		assertEquals("test 2 for sizeOfTempTable(): ", sizeAns2, x);	
+	}
+	
+	/**
+	 * test to see if printTempTable() prints the table of Temporary URL links correctly.
+	 */
+	@Test
+	public void testPrintTempTable(){
+		file = new File("myDatabaseAnswers1.txt");  // set up databaseManager with NON-empty database.
+		DatabaseManager dm = new DatabaseManagerImpl(file);
+		
+		dm.printTempTable();
+	}
+	
+	
+	/**
+	 * a) test to see if retrieveNextWebNode() retrieves the correct link for processing from the 
+	 * Temporary URL table; b) Test Also that the database has been correctly amended so that the
+	 * retrieved WebNode in the database has a priority setting at 0; c)  Also Return Null on Empty
+	 * database
+	 * 
+	 */
+	@Test
+	public void testRetrieveNextWebNode(){
+		file = new File("myDatabaseAnswers1a.txt");  // set up databaseManager with NON-empty database.
+		fileB = new File("myDatabaseAnswers1b.txt"); // solution file containing correct retrieval.
+		DatabaseManager dm = new DatabaseManagerImpl(file);
+		WebNode wn = dm.retrieveNextWebNode();  
+		WebNode ansNode = new WebNodeImpl("http://bbc.co.uk",1); // solution for retrieved WebNode
+
+		assertEquals("Test retrieveNextWebNode() for correct PriorityNum", 
+					 ansNode.getPriorityNum(),wn.getPriorityNum());
+		assertEquals("Test retrieveNextWebNode() for correct Web Link", 
+				 ansNode.getWebLink(),wn.getWebLink());
+		try {
+			assertEquals("The database has been correctly amended post retrieval", 
+				    FileUtils.readFileToString(file, "utf-8"), 
+				    FileUtils.readFileToString(fileB, "utf-8"));
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		fileC = new File("emptyDatabase.txt");  // set up databaseManager with empty database.
+		DatabaseManager dmEmpty = new DatabaseManagerImpl(file);
+		wn = dmEmpty.retrieveNextWebNode();
+		
+		assertEquals("Test retrieveNextWebNode() returns Null for emptyDatabase", 
+				 null,wn);
 	}
 
 }
