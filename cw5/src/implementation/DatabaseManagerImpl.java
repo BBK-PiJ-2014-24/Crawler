a5b55451a2e1c6794340d94fd40663028f7f7f04
Stewart Newnham <git@newnham.com>
2015-04-03 18:53:54 +0100

    DatabaseManager Impl - Implemented getter, getDatabaseFile().

--- cw5/src/implementation/DatabaseManagerImpl.java
+++ cw5/src/implementation/DatabaseManagerImpl.java
@@ -1,9 +1,11 @@
 package implementation;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
@@ -25,6 +27,11 @@ public class DatabaseManagerImpl implements DatabaseManager {
 	
 	public DatabaseManagerImpl(File file){
 		this.databaseFile = file;
+	}
+	
+	// getter
+	public File getDatabaseFile(){
+		return this.databaseFile;
 	}
 	
 	// stringToWebNode(String s)
@@ -82,13 +89,13 @@ public class DatabaseManagerImpl implements DatabaseManager {
 			
 			while((line=br.readLine()) != null){
 				if(line.equals("END")){
-					System.out.println("Find END");
+					//System.out.println("Find END");
 					break;
 				}
 				else{
-					System.out.println(line);
+					//System.out.println(line);
 					numWebNodes++;
-					System.out.println("numWebNodes " + numWebNodes);
+					//System.out.println("numWebNodes " + numWebNodes);
 				}
 				
 			}	
@@ -112,6 +119,105 @@ public class DatabaseManagerImpl implements DatabaseManager {
 		}
 		
 		return 0;
+	}
+
+	// printTempTable()
+	// ----------------
+	@Override
+	public void printTempTable() {
+		BufferedReader br = null;
+	
+		try{
+			FileReader fr= new FileReader(databaseFile);
+			br = new BufferedReader(fr);
+			String line;
+			
+			while((line=br.readLine()) != null){
+				if(line.equals("END")){
+					//System.out.println("Find END");
+					break;
+				}
+				else{
+					System.out.println(line);
+				}	
+			}	
+		}
+		catch(FileNotFoundException ex1){
+			ex1.printStackTrace();
+		}
+		catch(IOException ex2){
+			ex2.printStackTrace();
+		}
+		finally{
+			try{
+				br.close();
+			}
+			catch(IOException ex3){
+				ex3.printStackTrace();
+			}
+		}
+	}
+	
+	
+	// retrieveNextWebNode()
+	// ---------------------
+	@Override
+	public WebNode retrieveNextWebNode() {
+		boolean found = false;  //flag for finding first WebNode in temp Table of database
+		File tempFile = new File("tempDatabase.txt");  // temp Database to write to with amendments
+		FileWriter fw = null; 
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		WebNode retrievedNode = null;  // retrieved WebNode
+		
+		
+		try{
+			FileReader fr= new FileReader(databaseFile);
+			br = new BufferedReader(fr);
+			fw = new FileWriter(tempFile);
+			bw = new BufferedWriter(fw);
+			
+			String line;
+			
+			line = br.readLine();  // copy and paste headers
+			bw.write(line);
+			line = br.readLine();
+			bw.write(line);
+			
+			while((line=br.readLine()) != null){
+				StringTokenizer st = new StringTokenizer(line);
+				String tok1 = st.nextToken();
+				if(!tok1.equals("0") && found == false){     // if found FIRST non-zero priority
+					String tok2 = st.nextToken();
+					String replaceLine = "\t0\t\t\t" + tok2;   // amend line
+					bw.write(replaceLine + "\n");				    // write to file
+					found = true;							// set flag
+					retrievedNode = new WebNodeImpl(tok2,Integer.parseInt(tok1));  // create WebNode
+				}
+				else{
+					bw.write(line + "\n");   // copy and paste line
+				}						
+			}		
+		}
+		catch(FileNotFoundException ex1){
+			ex1.printStackTrace();
+		}
+		catch(IOException ex2){
+			ex2.printStackTrace();
+		}
+		finally{
+			try{
+				br.close();
+				bw.close();
+				this.databaseFile = tempFile;
+				tempFile.delete();   // delete temporaryFile
+				return retrievedNode;
+			}
+			catch(IOException ex3){
+				ex3.printStackTrace();
+			}
+		}
+		return null;
 	}
 
 }
