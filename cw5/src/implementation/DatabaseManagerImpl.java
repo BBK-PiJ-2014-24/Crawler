22c74bc526007bc9f5bff0d2c2f4480c4a4b4e0a
Stewart Newnham <git@newnham.com>
2015-04-03 12:37:25 +0100

    DatabaseManager Impl - First implementation of stringToWebNode()

---
+++ cw5/src/implementation/DatabaseManagerImpl.java
@@ -0,0 +1,45 @@
+package implementation;
+
+import java.util.StringTokenizer;
+
+import iinterface.DatabaseManager;
+import iinterface.WebNode;
+
+public class DatabaseManagerImpl implements DatabaseManager {
+
+	@Override
+	public WebNode stringToWebNode(String s) throws IllegalArgumentException {
+		
+		StringTokenizer st1 = new StringTokenizer(s,"\t");
+		String tok1;
+		String tok2;
+		WebNode wn = null;
+		
+		if(st1.countTokens() == 2){
+			tok1 = st1.nextToken();
+			tok2 = st1.nextToken();
+		}
+		else{
+			StringTokenizer st2 = new StringTokenizer(s," ");
+			if(st2.countTokens() == 2){
+				tok1 = st1.nextToken();
+				tok2 = st1.nextToken();
+			}
+			else{
+				throw new IllegalArgumentException();
+			}		
+		}
+		
+		try{
+			int priorityNum = Integer.parseInt(tok1);
+			String webPage = tok2;
+			wn = new WebNodeImpl(webPage, priorityNum);
+		}
+		catch(IllegalArgumentException ex){
+			ex.printStackTrace();
+		}
+	
+		return wn;
+	}
+
+}
